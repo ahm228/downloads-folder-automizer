56286aa58039280e871ebff894026b343fa6cc34
ahm228 <94765616+ahm228@users.noreply.github.com>
2023-10-08 10:39:03 -0500

    Update automateDownloads.py
    Function Breakdown:
    The revised code defines two functions: move_file_to_folder and organize_folder. This modular approach makes the code more readable and maintainable.
    
    Error Handling:
    The move_file_to_folder function wraps the shutil.move() inside a try-except block. This will catch exceptions like file permission errors, files in use, or files already existing in the target location. If an error occurs, it's printed out to inform the user.
    
    Recursive Handling of Subfolders:
    The function organize_folder checks if a given item is a directory using os.path.isdir(). If it's a directory (or subfolder), the function calls itself recursively to organize the files within that subfolder as well. This way, nested directories will also get organized.
    
    Feedback on Moved Files:
    Instead of just printing that the Downloads folder is organized, the revised code gives feedback on the number of files that have been moved. This provides a more detailed insight into what the script accomplished.
    
    Eliminate Empty Folder Creation:
    Folders like "Images", "Audio", etc., are now created only if there's a file to be moved into them. This is managed by the move_file_to_folder function which checks the existence of the destination folder before moving a file and creates it if it's not already there.
    
    Simplified 'Others' Handling:
    The 'Others' category is not explicitly listed in the folders dictionary in the revised code. Instead, if no match is found for a file's extension, it's automatically moved to the 'Others' folder. This makes the logic simpler and more intuitive.
    
    Removal of the has_moved Flag:
    In the original code, the has_moved flag was used to check if a file had been moved to one of the predefined categories. In the revised version, this flag was eliminated. Instead, the code directly checks if the file was moved by examining the return value of the move_file_to_folder function.

--- automateDownloads.py
+++ automateDownloads.py
@@ -2,37 +2,53 @@ import os
 import shutil
 
 # Define the folder to organize
-download_folder = os.path.expanduser("~/Downloads")  # Adjust the path to your specific Downloads folder
+download_folder = os.path.expanduser("~/Downloads")
 
-# Create subfolders
+# Extensions to folder mapping
 folders = {
     'Images': ['.jpg', '.jpeg', '.png', '.gif', '.tiff', '.bmp', '.heif'],
     'Documents': ['.pdf', '.docx', '.doc', '.xlsx', '.xls', '.pptx', '.ppt', '.txt', '.md'],
     'Audio': ['.mp3', '.wav', '.m4a', '.flac', '.aac'],
     'Video': ['.mp4', '.mov', '.avi', '.mkv', '.flv'],
-    'Others': []
 }
 
-# Create subfolders in the Downloads folder
-for folder, extensions in folders.items():
-    folder_path = os.path.join(download_folder, folder)
+def move_file_to_folder(file_path, folder_name):
+    """Move a file to a specific folder, creating the folder if it doesn't exist."""
+    folder_path = os.path.join(download_folder, folder_name)
     if not os.path.exists(folder_path):
         os.makedirs(folder_path)
+    try:
+        shutil.move(file_path, os.path.join(folder_path, os.path.basename(file_path)))
+        return True
+    except Exception as e:
+        print(f"Error moving {file_path} to {folder_name}. Reason: {e}")
+        return False
 
-# Loop to move files to respective folders
-for filename in os.listdir(download_folder):
-    if os.path.isfile(os.path.join(download_folder, filename)):
-        file_ext = os.path.splitext(filename)[1]
+def organize_folder(dir_path):
+    moved_count = 0
+    for item_name in os.listdir(dir_path):
+        item_path = os.path.join(dir_path, item_name)
 
+        # If it's a directory, recurse into it
+        if os.path.isdir(item_path):
+            moved_count += organize_folder(item_path)
+            continue
+
+        file_ext = os.path.splitext(item_name)[1]
         has_moved = False
 
         for folder, extensions in folders.items():
             if file_ext in extensions:
-                shutil.move(os.path.join(download_folder, filename), os.path.join(download_folder, folder, filename))
-                has_moved = True
+                if move_file_to_folder(item_path, folder):
+                    has_moved = True
+                    moved_count += 1
                 break
 
         if not has_moved:
-            shutil.move(os.path.join(download_folder, filename), os.path.join(download_folder, 'Others', filename))
+            if move_file_to_folder(item_path, 'Others'):
+                moved_count += 1
 
-print("Downloads folder organized.")
+    return moved_count
+
+moved_files = organize_folder(download_folder)
+print(f"{moved_files} files have been organized.")
